d2faa418a8cbf2bf827168c79c6f9c8fd4e8ccfa
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2020-03-07 17:22:04 +0100

    Virtual device simulator random data stream emitter for sensors.

---
+++ virtual-device-simulator/src/main/java/com/unloadbrain/assignment/relay42/virtualdevices/service/RandomDataStreamEmitter.java
@@ -0,0 +1,52 @@
+package com.unloadbrain.assignment.relay42.virtualdevices.service;
+
+import java.util.Random;
+import java.util.SplittableRandom;
+import java.util.function.Consumer;
+
+public class RandomDataStreamEmitter {
+
+    private final static Random random = new Random();
+
+    public void emitRandomIntsStream(int randomNumberOrigin, int randomNumberBound, int randomSleepBound,
+                                     final Consumer<Integer> consumer) {
+
+        Thread thread = new Thread(String.format("RandomIntsDataStreamThread")) {
+            public void run() {
+                new SplittableRandom()
+                        .ints(randomNumberOrigin, randomNumberBound)
+                        .forEach(reading -> {
+                            randomSleep(randomSleepBound);
+                            consumer.accept(reading);
+                        });
+            }
+        };
+
+        thread.start();
+    }
+
+    public void emitRandomDoubleStream(double randomNumberOrigin, double randomNumberBound, int randomSleepBound,
+                                       final Consumer<Double> consumer) {
+
+        Thread thread = new Thread(String.format("RandomDoublesDataStreamThread")) {
+            public void run() {
+                new SplittableRandom()
+                        .doubles(randomNumberOrigin, randomNumberBound)
+                        .forEach(reading -> {
+                            randomSleep(randomSleepBound);
+                            consumer.accept(reading);
+                        });
+            }
+        };
+
+        thread.start();
+    }
+
+    private void randomSleep(int randomSleepBound) {
+        try {
+            Thread.sleep(random.nextInt(randomSleepBound));
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Thread sleep interrupted!");
+        }
+    }
+}
