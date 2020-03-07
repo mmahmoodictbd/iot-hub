a2e9b5e73448b8af6eec1994878d4c5fc2f95147
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2020-03-07 17:25:44 +0100

    Virtual device simulator context load IT and mock inliner mockito extension.

---
+++ virtual-device-simulator/src/integration-test/java/com/unloadbrain/assignment/relay42/virtualdevices/ContextLoadIT.java
@@ -0,0 +1,34 @@
+package com.unloadbrain.assignment.relay42.virtualdevices;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.mockito.Mockito.mock;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {Application.class, ContextLoadIT.TestConfig.class})
+@ActiveProfiles("it")
+public class ContextLoadIT {
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Configuration
+    static class TestConfig {
+
+        // The reason behind the mocking is actual MQTT client tries to connect with the broker.
+        @Primary
+        @Bean
+        public MqttClient mqttClient() {
+            return mock(MqttClient.class);
+        }
+    }
+}
