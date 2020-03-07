98ca6139499d3154bed0998346f18785098803bc
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2020-03-07 14:20:03 +0100

    Kafka influxdb sink.

---
+++ kafka-connect-influxdb-sink/src/main/java/com/unloadbrain/kafka/connect/influxdb/sink/KafkaConnectInfluxDBSink.java
@@ -0,0 +1,41 @@
+package com.unloadbrain.kafka.connect.influxdb.sink;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.wnameless.json.flattener.JsonFlattener;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.influxdb.InfluxDB;
+import org.influxdb.dto.Point;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@AllArgsConstructor
+public class KafkaConnectInfluxDBSink {
+
+    private final InfluxDB influxDB;
+    private final ObjectMapper mapper;
+
+    @KafkaListener(topics = "#{'${kafka.connector.topics}'.split(',')}")
+    public void kafkaToInfluxDB(@Header(KafkaHeaders.RECEIVED_TOPIC) String topic, @Payload String payload) {
+
+        String flattenedPayload = JsonFlattener.flatten(payload);
+
+        Map<String, Object> fields = null;
+        try {
+            fields = mapper.readValue(flattenedPayload, HashMap.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Could not transform json to HashMap.");
+        }
+
+        Point point = Point.measurement(topic).fields(fields).build();
+        influxDB.write(point);
+    }
+
+}
